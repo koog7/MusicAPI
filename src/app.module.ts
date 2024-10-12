9fe3df054d23750b5aec317e43cfc25b2bfd6080
Konstantin <k.ognev709@mail.ru>
2024-10-12 12:47:25 +0600

    Подготовлена схема пользователей бд

--- src/app.module.ts
+++ src/app.module.ts
@@ -8,6 +8,7 @@ import { AlbumController } from './album/album.controller';
 import { Album, AlbumSchema } from './schemas/album.schema';
 import { TrackController } from './track/track.controller';
 import { Track, TrackSchema } from './schemas/tracks.schema';
+import { User, UserSchema } from './schemas/user.schema';
 
 @Module({
   imports: [
@@ -16,6 +17,7 @@ import { Track, TrackSchema } from './schemas/tracks.schema';
         {name: Artist.name , schema: ArtistSchema},
         {name: Album.name , schema: AlbumSchema},
         {name: Track.name , schema: TrackSchema},
+        {name: User.name , schema: UserSchema},
       ])
   ],
   controllers: [AppController, ArtistController, AlbumController, TrackController],
---
+++ src/schemas/user.schema.ts
@@ -0,0 +1,48 @@
+import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
+import bcrypt from 'bcrypt';
+import {Document} from 'mongoose';
+
+const SALT_WORK_FACTOR = 10;
+
+export type UserDocument = User & Document;
+
+@Schema()
+export class User {
+  @Prop({required: true, unique: true})
+  username: string;
+
+  @Prop({required: true})
+  password: string;
+
+  @Prop({required: true})
+  token: string;
+
+  @Prop({required: true , enum: ['user' , 'admin'] , default: 'user'})
+  role:string
+}
+
+export const UserSchema = SchemaFactory.createForClass(User);
+
+UserSchema.methods.generateToken = function generateToken() {
+  this.token = crypto.randomUUID();
+}
+
+UserSchema.methods.checkPassword = function (password: string) {
+  return bcrypt.compare(password, this.password);
+}
+
+UserSchema.pre<UserDocument>('save', async function (){
+  if(!this.isModified('password')){
+    return;
+  }
+
+  const salt = await bcrypt.genSalt(SALT_WORK_FACTOR);
+  this.password = await bcrypt.hash(this.password , salt)
+})
+
+UserSchema.set('toJSON', {
+  transform: (_doc , ret) =>{
+    delete ret.password;
+    return ret;
+  }
+})
