2f9ccd446bbe20cb9311f6e5dfa8b5ab36509d31
Konstantin <k.ognev709@mail.ru>
2024-10-13 16:37:28 +0600

    Добавлен логаут пользователя

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,11 +1,11 @@
-import { Body, Controller, Post, Req, UseGuards } from '@nestjs/common';
+import { Body, Controller, Delete, Post, Req, UseGuards } from '@nestjs/common';
 import { User, UserDocument } from '../schemas/user.schema';
 import { Model } from 'mongoose';
 import { InjectModel } from '@nestjs/mongoose';
 import { AuthGuard } from '@nestjs/passport';
 import { TokenAuthGuard } from '../auth/token-auth.guard';
 import { use } from 'passport';
-
+import {Request, Response} from 'express';
 export interface AuthRequest extends Request {
   user: {
     username: string;
@@ -43,5 +43,32 @@ export class UsersController {
   async secret(@Req() req: AuthRequest){
     const user = req.user as UserDocument;
     return {message:'success' , user: user.username}
+  }
+
+  @Delete('sessions')
+  async delete(@Req() req: Request){
+    const getToken = req.get('Authorization');
+    console.log(getToken);
+    if(!getToken){
+      return null;
+    }
+
+    const [, token] = getToken.split(' ')
+
+    if(!token){
+      return null;
+    }
+    console.log(token);
+    const user = await this.userModel.findOne({token:token});
+
+    if(!user){
+      return null;
+    }
+    console.log(user);
+    user.generateToken()
+
+    await user.save();
+
+    return { message: 'User logged out successfully' }
   }
 }
